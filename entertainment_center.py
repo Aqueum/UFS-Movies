b2a59258f51b1cd8d196b895cef69d62e463da49
Martin Currie <martin@aqueum.com>
2017-05-26 12:42:14 +0100

    Rename to match project specification file names

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,16 @@
-import movies
+import media
 import fresh_tomatoes
 
 # instantiation of my three favourite movies:
-shallow_grave = movies.Movie("Shallow Grave", "Bad housemates",
+shallow_grave = media.Movie("Shallow Grave", "Bad housemates",
                              "https://upload.wikimedia.org/wikipedia/en/2/23/Shallow.jpg",
                              "https://www.youtube.com/watch?v=HbxNej_MJlw")
 
-three_colours_blue = movies.Movie("Three Colours: Blue", "Liberty post-tragedy",
+three_colours_blue = media.Movie("Three Colours: Blue", "Liberty post-tragedy",
                                   "https://upload.wikimedia.org/wikipedia/en/2/2c/Bluevidcov.jpg",
                                   "https://www.youtube.com/watch?v=Hxu6my_t4pM")
 
-casablanca = movies.Movie("Casablanca", "How to buy a pub",
+casablanca = media.Movie("Casablanca", "How to buy a pub",
                           "https://upload.wikimedia.org/wikipedia/commons/b/b3/CasablancaPoster-Gold.jpg",
                           "https://www.youtube.com/watch?v=S9ID5DHsX8g")
 
