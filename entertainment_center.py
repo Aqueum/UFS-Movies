60c4ac60d25811f7ac9a9937bcd1cd8a3087d27a
Martin Currie <martin@aqueum.com>
2017-05-26 12:47:50 +0100

    Fix overindentation of line continuations

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,16 +3,16 @@ import fresh_tomatoes
 
 # instantiation of my three favourite movies:
 shallow_grave = media.Movie("Shallow Grave", "Bad housemates",
-                             "https://upload.wikimedia.org/wikipedia/en/2/23/Shallow.jpg",
-                             "https://www.youtube.com/watch?v=HbxNej_MJlw")
+                            "https://upload.wikimedia.org/wikipedia/en/2/23/Shallow.jpg",
+                            "https://www.youtube.com/watch?v=HbxNej_MJlw")
 
 three_colours_blue = media.Movie("Three Colours: Blue", "Liberty post-tragedy",
-                                  "https://upload.wikimedia.org/wikipedia/en/2/2c/Bluevidcov.jpg",
-                                  "https://www.youtube.com/watch?v=Hxu6my_t4pM")
+                                 "https://upload.wikimedia.org/wikipedia/en/2/2c/Bluevidcov.jpg",
+                                 "https://www.youtube.com/watch?v=Hxu6my_t4pM")
 
 casablanca = media.Movie("Casablanca", "How to buy a pub",
-                          "https://upload.wikimedia.org/wikipedia/commons/b/b3/CasablancaPoster-Gold.jpg",
-                          "https://www.youtube.com/watch?v=S9ID5DHsX8g")
+                         "https://upload.wikimedia.org/wikipedia/commons/b/b3/CasablancaPoster-Gold.jpg",
+                         "https://www.youtube.com/watch?v=S9ID5DHsX8g")
 
 # collection of my three favourite movie interfaces into movie_list
 movie_list = [shallow_grave, three_colours_blue, casablanca]
